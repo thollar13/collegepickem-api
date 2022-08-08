38a8adada7c77a4e168163eeda348bef943fe9fc
Thomas Hollar <hollart13@gmail.com>
2022-08-08 15:41:06 -0400

    troubleshooting

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const cors = require('cors');
 
 const app = express();
 
-// const db = require('./queries')
-
-// app.get('/api/users', db.getUsers)
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- config/database.js
+++ config/database.js
@@ -1,22 +1,12 @@
+require("dotenv").config();
 const Pool = require('pg').Pool
 
-let pool
-if(process.env.NODE_ENV === 'production') {
-  pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  })
-} else {
-  pool = new Pool({
-    user: 'thomashollar',
-    host: 'localhost',
-    database: 'collegepickems',
-    password: 'password',
-    port: 5432,
-  }) 
-}
+const pool = new Pool({
+    user: process.env.DB_USER || 'thomashollar',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_DATABASE || 'collegepickems',
+    password: process.env.DB_PASSWORD || 'password',
+    port: process.env.DB_PORT || 5432,
+})
 
 module.exports = pool
