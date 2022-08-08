182f878bd11c710edaf9afdb8870b100b3c5fd21
Thomas Hollar <hollart13@gmail.com>
2022-08-08 16:09:42 -0400

    rds support troubleshoot

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'thomashollar',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_DATABASE || 'collegepickems',
-    password: process.env.DB_PASSWORD || 'password',
-    port: process.env.PORT || 5432,
+    user: process.env.RDS_USERNAME || 'thomashollar',
+    host: process.env.RDS_HOSTNAME || 'localhost',
+    database: process.env.RDS_DB_NAME || 'collegepickems',
+    password: process.env.RDS_PASSWORD || 'password',
+    port: process.env.RDS_PORT || 5432,
 })
 
 module.exports = pool
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const server = http.createServer(app);
 
 const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
+const port = process.env.RDS_PORT || API_PORT;
 
 // server listening 
 server.listen(port, () => {
