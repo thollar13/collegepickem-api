e5d4f24200625062fd424ecdb27504e7d5dec49c
Thomas Hollar <hollart13@gmail.com>
2022-08-08 14:31:53 -0400

    adding prod environment variables

--- config/database.js
+++ config/database.js
@@ -1,10 +1,22 @@
 const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'thomashollar',
-  host: 'localhost',
-  database: 'collegepickems',
-  password: 'password',
-  port: 5432,
-})
+
+let pool
+if(process.env.NODE_ENV === 'production') {
+  pool = new Pool({
+    user: process.env.POSTGRE_DB_USER,
+    host: process.env.POSTGRE_DB_HOST,
+    database: process.env.POSTGRE_DB_DATABASE,
+    password: process.env.POSTGRE_DB_PASSWORD,
+    port: process.env.POSTGRE_DB_PORT,
+  })
+} else {
+  pool = new Pool({
+    user: 'thomashollar',
+    host: 'localhost',
+    database: 'collegepickems',
+    password: 'password',
+    port: 5432,
+  }) 
+}
 
 module.exports = pool
