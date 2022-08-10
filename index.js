355c2677c2d0338c306582b1c8ca484637211421
Thomas Hollar <hollart13@gmail.com>
2022-08-10 09:36:58 -0400

    changing port stuff

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = require("./app");
 const server = http.createServer(app);
 
 const { API_PORT } = process.env;
-const port = process.env.RDS_PORT || API_PORT;
 
 // server listening 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+server.listen(API_PORT, () => {
+  console.log(`Server running on port ${API_PORT}`);
 });
